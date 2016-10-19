51aa6937f2d743c2fc11a2ca3dac8f1b6700dc89
Stas Khokhryakov <s.hohryakov@rtc.ru>
2016-10-19 15:46:59 +0300

    Изменена генерация сетки.

--- Pixie/PatternGenerator.cs
+++ Pixie/PatternGenerator.cs
@@ -25,10 +25,10 @@ namespace Pixie
         /// <returns>production ready bitmap</returns>
         public Bitmap GeneratePattern(int patternWidthCount, int patternHeightCount)
         {
-            var pattentWidth = patternWidthCount*_settings.SymbolWidth +
-                               (patternWidthCount - 1)*_settings.DelimeterWidth;
-            var patternHeight = patternHeightCount*_settings.SymbolHeight +
-                                (patternHeightCount - 1)*_settings.DelimeterHeight;
+            var pattentWidth = patternWidthCount * _settings.SymbolWidth +
+                               (patternWidthCount - 1) * _settings.DelimeterWidth;
+            var patternHeight = patternHeightCount * _settings.SymbolHeight +
+                                (patternHeightCount - 1) * _settings.DelimeterHeight;
             var pattern = new Bitmap(pattentWidth, patternHeight);
 
             FillBackground(pattern);
@@ -44,13 +44,9 @@ namespace Pixie
         /// <param name="pattern">bitmap to fill</param>
         private void FillBackground(Bitmap pattern)
         {
-            for (int j = 0; j < pattern.Height; j++)
-            {
-                for (int i = 0; i < pattern.Width; i++)
-                {
-                   pattern.SetPixel(i,j, BackGround);
-                }
-            }
+            var g = Graphics.FromImage(pattern);
+            g.FillRectangle(new SolidBrush(BackGround), new Rectangle(0, 0, pattern.Width, pattern.Height));
+
         }
 
         /// <summary>
@@ -59,19 +55,12 @@ namespace Pixie
         /// <param name="pattern">bitmap to draw in</param>
         private void DrawHorizontalLines(Bitmap pattern)
         {
-            for (int i = 0; i < pattern.Width; i++)
+            var g = Graphics.FromImage(pattern);
+            for (var i = _settings.SymbolHeight; i < pattern.Height; i += _settings.SymbolHeight + _settings.DelimeterHeight)
             {
-                for (int j = _settings.SymbolHeight; j < pattern.Height; j += _settings.SymbolHeight)
-                {
-                    var linePixelsLeft = _settings.DelimeterHeight;
-                    while (linePixelsLeft > 0)
-                    {
-                        pattern.SetPixel(i, j, _delimeterColor);
-                        j++;
-                        linePixelsLeft--;
-                    }
-                }
+                g.DrawLine(new Pen(_delimeterColor, _settings.DelimeterHeight), 0.0f, i, pattern.Width, i);
             }
+
         }
 
         /// <summary>
@@ -80,18 +69,10 @@ namespace Pixie
         /// <param name="pattern">bitmap to draw in</param>
         private void DrawVerticalLines(Bitmap pattern)
         {
-            for (int j = 0; j < pattern.Height; j++)
+            var g = Graphics.FromImage(pattern);
+            for (var i = _settings.SymbolWidth; i < pattern.Width; i += _settings.SymbolWidth + _settings.DelimeterWidth)
             {
-                for (int i = _settings.SymbolWidth; i < pattern.Width; i+=_settings.SymbolWidth)
-                {
-                    var linePixelsLeft = _settings.DelimeterWidth;
-                    while (linePixelsLeft > 0)
-                    {
-                        pattern.SetPixel(i, j, _delimeterColor);
-                        i++;
-                        linePixelsLeft--;
-                    }
-                }
+                g.DrawLine(new Pen(_delimeterColor, _settings.DelimeterWidth), i, 0, i, pattern.Height);
             }
         }
     }
