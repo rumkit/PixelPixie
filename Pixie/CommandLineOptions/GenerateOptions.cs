61266778fb03174b73cf646a10cf441ed276ed1e
Amomum <amomum@yandex.ru>
2017-09-27 01:15:03 +0300

    Added help for -n option. fixed #11

--- Pixie/CommandLineOptions/GenerateOptions.cs
+++ Pixie/CommandLineOptions/GenerateOptions.cs
@@ -13,7 +13,7 @@ namespace Pixie
         [Option('o', "output", HelpText = "output file name", DefaultValue = "output.png")]
         public override string OutputFileName { get; set; }
 
-        [Option('n', "enumerate", HelpText = "draw grid rows and columns numbers", DefaultValue = Pixie.EnumerationStyle.None)]
+        [Option('n', "enumerate", HelpText = "draw grid rows and columns numbers. Use '-n Hex' or '-n Decimal'", DefaultValue = Pixie.EnumerationStyle.None)]
         public EnumerationStyle EnumerationStyle { get; set; }
     }
 }
